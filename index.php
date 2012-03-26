1809107a08ef307584de264b947d686e44eb8870
Gavin Towey <gtowey@dba1001.ve.box.net>
2012-03-26 12:12:29 -0700

    removed todo items, added to git issues

--- index.php
+++ index.php
@@ -11,15 +11,7 @@
 set_include_path( get_include_path() . PATH_SEPARATOR . "./lib");
 require "Helpers.php";
 require "Anemometer.php";
-/*
-	* move these to github
-	* todo: implement exra fields ... somehow<br> or remove extra fields for version 1
-	* todo: add reviewed_status to search fields ... tab for extended search?<br>
-	* todo: possible report for reviewed (aggregate by reviewd type, reviwed/unreviewed / new  or old, etc)<br>
-	* todo: cleanup MySQLReport object<br>
-	* todo: document / organise it all
-	* maybe make explain / create / etc ajax requests so we don't do requests when nobody looks at it
-*/
+
 error_reporting(E_ALL);
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
