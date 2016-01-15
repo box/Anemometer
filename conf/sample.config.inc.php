5016f454c15d54112c59bd3e507460fe803e5059
Colin Hutchinson <chutchinson@freshbooks.com>
2016-01-15 15:17:32 -0500

    Moved Docker stuff to it's own directory

--- conf/sample.config.inc.php
+++ conf/sample.config.inc.php
@@ -33,11 +33,11 @@
  *
  */
 $conf['datasources']['localhost'] = array(
-	'host'	=> getenv('ANEMOMETER_MYSQL_1_PORT_3306_TCP_ADDR') ?: 'localhost',
+	'host'	=> getenv('DB_PORT_3306_TCP_ADDR') ?: 'localhost',
 	'port'	=> 3306,
 	'db'	=> 'slow_query_log',
-	'user'	=> getenv('ANEMOMETER_MYSQL_1_ENV_MYSQL_USER') ?: 'root',
-	'password' => getenv('ANEMOMETER_MYSQL_1_ENV_MYSQL_PASS') ?: '',
+	'user'	=> getenv('DB_ENV_MYSQL_USER') ?: 'root',
+	'password' => getenv('DB_ENV_MYSQL_PASS') ?: '',
 	'tables' => array(
 		'global_query_review' => 'fact',
 		'global_query_review_history' => 'dimension'
