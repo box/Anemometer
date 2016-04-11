c346f31a85be9c071e4fa23c86c9d31d09c1aa29
3manuek <3manuek@esdebian.org>
2016-04-11 18:59:43 -0300

    Fixes

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,7 @@
 sudo -s 
 
 # base packages, including percona mysql repo
+yum install -y kernel-devel-`uname -r` gcc make perl bzip2
 yum install -y http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
 yum install -y Percona-Server-client-56 Percona-Server-shared-56 Percona-Server-server-56
 yum install -y httpd php php-mysql php-bcmath wget git
