78621b6599b3a170567382e39573a2a51af1de66
3manuek <3manuek@esdebian.org>
2016-04-13 21:24:50 -0300

    Fixes and additions on the documentation

--- vagrant/README.md
+++ vagrant/README.md
@@ -1,18 +1,31 @@
-## Migrated to Centos
+## Migrated to Centos7
 
+Reference:
 https://github.com/mitchellh/vagrant/issues/6497
 
-Adding:
+
+Added in the `Vagrantfile`:
 
 ```
 config.vm.synced_folder ".", "/vagrant", type: "nfs"
 ```
+
 
 I thought it was: https://www.virtualbox.org/ticket/12879
 
 
 Solution was:
 
+```
 vagrant plugin install vagrant-vbguest
+```
 
+## How to setup the vagrant machine
 
+```
+mkdir vagrant
+git clone git@github.com:3manuek/Anemometer.git anemometer
+ln -s anemometer/vagrant/Vagrantfile Vagrantfile
+ln -s anemometer/vagrant/bootstrap.sh bootstrap.sh
+vagrant up
+```
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo -s
 
+echo "Package installation; can take several minutes."
 # base packages, including percona mysql repo
 yum update -y
 yum install -y kernel-devel-`uname -r` gcc make perl bzip2
@@ -13,28 +14,14 @@ yum install -y httpd php php-mysql php-bcmath wget git
 wget http://mirror.symnds.com/distributions/gf/el/7/plus/x86_64/rsync-3.1.1-6.gf.el7.x86_64.rpm
 rpm -Uvh rsync-3.1.1-6.gf.el7.x86_64.rpm
 
-# install percona toolkit
 yum install -y perl-DBD-MySQL perl-Time-HiRes perl-IO-Socket-SSL
 wget -q "http://www.percona.com/redir/downloads/percona-toolkit/2.2.10/RPM/percona-toolkit-2.2.10-1.noarch.rpm"
 rpm -i percona-toolkit-2.2.10-1.noarch.rpm
 
-#sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux
+
+echo "Disabling SELinux"
 sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
-
 setenforce 0
-
-systemctl start mysqld.service
-systemctl start httpd.service
-
-git clone https://github.com/3manuek/Anemometer.git anemometer
-
-# setup symlink for apache & install anemometer files
-#ln -s /vagrant/anemometer  /var/www/html/anemometer
-
-[[ ! -h  /var/www/html/anemometer ]] && ln -s /home/vagrant/anemometer /var/www/html/anemometer
-
-#mysql -u root < /vagrant/anemometer/install.sql
-mysql -u root < anemometer/mysql56-install.sql 2> /dev/null
 
 
 # create my.cnf for access
@@ -43,8 +30,27 @@ cat << EOF > /home/vagrant/.my.cnf
 user=root
 EOF
 
+
+echo "Starting services"
+systemctl start mysqld.service
+systemctl start httpd.service
+
+# Creating symbolic links ocurred in permission issues.
+echo "Cloning anemometer"
+git clone https://github.com/3manuek/Anemometer.git /var/www/html/anemometer
+
 # add cron for collection script
 cat << EOF > /home/vagrant/crontab
 */5 * * * * /vagrant/anemometer/scripts/anemometer_collect.sh --interval 15 --history-db-host localhost --defaults-file /home/vagrant/.my.cnf --history-defaults-file /home/vagrant/.my.cnf
 EOF
 crontab -u root /home/vagrant/crontab
+
+
+# setup symlink for apache & install anemometer files
+# ln -s /vagrant/anemometer  /var/www/html/anemometer
+
+# [[ ! -h  /var/www/html/anemometer ]] && ln -s /home/vagrant/anemometer /var/www/html/anemometer
+
+#mysql -u root < /vagrant/anemometer/install.sql
+echo "Installing DB ..."
+mysql -u root < anemometer/mysql56-install.sql 2> /dev/null
