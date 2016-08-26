ef435fd2d2a5cb1a2c6c3c2ee3d7ed81132171c8
Oleksandr Kuzminskyi <okuzminskyi@box.com>
2016-08-26 10:58:09 -0700

    Dockerize Anemometer

--- mysql56-save_history.sql
+++ mysql56-save_history.sql
@@ -1,11 +1,28 @@
 USE slow_query_log;
 
-CREATE TEMPORARY TABLE `statements_temp` SELECT * FROM performance_schema.events_statements_summary_by_digest;
+CREATE TEMPORARY TABLE `slow_query_log`.`statements_temp`
+SELECT * FROM performance_schema.events_statements_summary_by_digest;
 
-INSERT INTO events_statements (DIGEST, DIGEST_TEXT, first_seen, last_seen) SELECT DIGEST, DIGEST_TEXT, FIRST_SEEN, LAST_SEEN FROM statements_temp ON DUPLICATE KEY UPDATE first_seen=LEAST(VALUES(events_statements.first_seen), events_statements.first_seen), last_seen=GREATEST(VALUES(events_statements.last_seen),events_statements.last_seen);
+INSERT INTO `slow_query_log`.`events_statements` (DIGEST, DIGEST_TEXT, first_seen, last_seen)
+SELECT DIGEST, DIGEST_TEXT, FIRST_SEEN, LAST_SEEN
+FROM `slow_query_log`.`statements_temp`
+  ON DUPLICATE KEY UPDATE
+    first_seen = LEAST(VALUES(`slow_query_log`.`events_statements`.first_seen),
+                              `slow_query_log`.`events_statements`.first_seen),
+    last_seen = GREATEST(VALUES(`slow_query_log`.`events_statements`.last_seen),
+                                `slow_query_log`.`events_statements`.last_seen);
 
-SELECT CONCAT('INSERT IGNORE INTO events_statements_history (', GROUP_CONCAT(DISTINCT a.column_name),',hostname) SELECT ', GROUP_CONCAT(DISTINCT a.column_name),', @@hostname FROM statements_temp') INTO @stmt from information_schema.columns a JOIN information_schema.columns b ON a.column_name=b.column_name and b.table_name='events_statements_history'  where a.table_schema='performance_schema' and a.table_name='events_statements_summary_by_digest';
+SELECT CONCAT('INSERT IGNORE INTO events_statements_history (',
+              GROUP_CONCAT(DISTINCT a.column_name),',hostname) SELECT ',
+              GROUP_CONCAT(DISTINCT a.column_name),', @@hostname FROM statements_temp')
+              INTO @stmt
+FROM information_schema.columns a
+  JOIN information_schema.columns b
+    ON a.column_name=b.column_name AND b.table_name='events_statements_history'
+WHERE a.table_schema='performance_schema' AND a.table_name='events_statements_summary_by_digest';
+
 PREPARE stmt FROM @stmt;
+
 EXECUTE stmt;
 
 DROP TABLE IF EXISTS statements_temp;
