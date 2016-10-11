681e3df0b4ff4f0e852c3f7604074cb29a7641c0
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2016-10-11 15:44:18 -0700

    Refactored tests for QueryExplain

---
+++ Makefile
@@ -0,0 +1,69 @@
+.PHONY: clean clean-test clean-build docs help test vagrant
+.DEFAULT_GOAL := help
+
+define PRINT_HELP_PYSCRIPT
+import re, sys
+
+for line in sys.stdin:
+	match = re.match(r'^([a-zA-Z_-]+):.*?## (.*)$$', line)
+	if match:
+		target, help = match.groups()
+		print("%-20s %s" % (target, help))
+endef
+export PRINT_HELP_PYSCRIPT
+
+define EXTRACT_PHPUNIT
+<?php
+$$phar = new Phar('vendor/phpunit.phar');
+$$phar->extractTo('vendor/phpunit');
+endef
+export EXTRACT_PHPUNIT
+
+
+help:
+	@python -c "$$PRINT_HELP_PYSCRIPT" < $(MAKEFILE_LIST)
+
+clean: clean-build clean-test ## remove all build, test, coverage and artifacts
+
+clean-build: ## remove build artifacts
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -f {} +
+
+clean-test: ## remove test and coverage artifacts
+	rm -fr .tox/
+	rm -f .coverage
+	rm -fr htmlcov/
+
+
+test: ## run tests quickly with the default PHP
+	phpunit test
+
+phpunit: vendor/phpunit
+
+vendor/phpunit: vendor/phpunit.phar
+	echo $$EXTRACT_PHPUNIT | php
+
+vendor/phpunit.phar: vendor 
+	wget -O vendor/phpunit.phar https://phar.phpunit.de/phpunit.phar
+
+vendor:
+	mkdir vendor
+
+vagrant: ## Start vagrant virtual machine
+	cd vagrant && vagrant up
+
+docs: phpdoc ## Update documentation
+	./vendor/phpdocumentor/phpdocumentor/bin/phpdoc -d lib -t docs
+
+phpdoc:
+	test -f ./vendor/phpdocumentor/phpdocumentor/bin/phpdoc || composer install
+
+composer: ## Install composer locally
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	php composer-setup.php -- --cafile /tmp/cacert.pem
+	php -r "unlink('composer-setup.php');"
+	php -r "unlink('/tmp/cacert.pem');"
--- test/QueryTableParserTest.php
+++ test/QueryTableParserTest.php
@@ -1,9 +1,13 @@
 <?php
-require_once 'lib/QueryTableParser.php';
+
+require_once __DIR__.'/../lib/QueryTableParser.php';
 
 class TestQueryTableParser extends PHPUnit_Framework_TestCase
 {
-    protected $_QueryTableParser = null;
+    /**
+     * @var QueryTableParser
+     */
+    protected $_QueryTableParser;
 
     public function setUp()
     {
@@ -20,55 +24,55 @@ class TestQueryTableParser extends PHPUnit_Framework_TestCase
         $sql = 'SELECT col1 from table01';
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testSelectWithComments() {
         $sql                     = '/* 123 * 456 */ SELECT col1 from table01 #214241*/';
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testSelectWithWhitespace() {
         $sql                     = "\t\t    SELECT       \t\t\t   col1 \t\t\t from   \t\t table01 \t\t\n";
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testDelete() {
         $sql                     = 'DELETE FROM table01 WHERE id = 123';
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testInsert() {
         $sql                     = 'INSERT INTO table01 (col1, col2, col3) VALUES (1, 2, 3), (4, 5, 6)';
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testInsertSelect() {
         $sql                     = 'INSERT INTO table01 SELECT * FROM table02';
         $expectedResult = array('table01','table02');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testUpdate() {
         $sql                     = 'UPDATE table01 SET col1 = 123 WHERE id = 456';
         $expectedResult = array('table01');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 
     public function testJoin() {
         $sql                     = 'SELECT col1 from table01 INNER JOIN table02 on table01.col2=table02.col3';
         $expectedResult = array('table01','table02');
         $result = $this->_QueryTableParser->parse($sql);
-        $this->assertEquals($expectedResult, $result);
+        static::assertEquals($expectedResult, $result);
     }
 }
